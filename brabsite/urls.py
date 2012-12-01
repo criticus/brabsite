14f5ef0779114c8d187b5389fff7e96d017c90a9
rostyvyg <rostyvyg@yahoo.com>
2012-12-01 15:17:55 -0500

    Tied user registration/Authentication framework with brabs

--- brabsite/urls.py
+++ brabsite/urls.py
@@ -2,16 +2,22 @@ from django.conf.urls import *
 from django.contrib import admin
 from django.views.generic import ListView, DetailView
 from brabs.models import Brab
-from brabs.views import BrabAddView, BrabDetailView
+from brabs.views import BrabAddView, BrabDetailView, BrabListView
+from django.http import HttpRequest
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-
+    url(r'^mybrabs/$', BrabListView.as_view(
+        paginate_by=25,
+        context_object_name="brab_list"),
+        name="my-brab-list-view"
+    ),
     url(r'^$', ListView.as_view(
         queryset=Brab.objects.all(),
+        paginate_by=25,
         context_object_name="brab_list"),
-        name="home"
+        name="all-brab-list-view"
     ),
     url(r'^addabrab/$', BrabAddView.as_view(),
         name='BrabAddView'
